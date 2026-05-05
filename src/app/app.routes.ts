fd428ca8453cdcbcb0991308c0307f1bc8aa12d2
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-05-05 22:29:02 +0300

    refactor(router): optimize lazy loading and enable global scroll restoration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,60 +1,80 @@
 import { Routes } from '@angular/router';
 import { Layout } from './shared/components/layout/layout';
-import { Login } from './features/auth/login/login';
-import { Register } from './features/auth/register/register';
 import { NotFound } from './shared/components/not-found/not-found';
+import { HomePage } from './pages/home/home';
 import { authGuard } from './core/guards/auth.guard';
 import { guestGuard } from './core/guards/guest.guard';
-import { HomePage } from './pages/home/home';
 
 export const routes: Routes = [
-  { path: 'login', canActivate: [guestGuard], component: Login },
-  { path: 'register', canActivate: [guestGuard], component: Register },
+  {
+    path: 'login',
+    canActivate: [guestGuard],
+    loadComponent: () => import('./features/auth/login/login').then((c) => c.Login),
+  },
+  {
+    path: 'register',
+    canActivate: [guestGuard],
+    loadComponent: () => import('./features/auth/register/register').then((c) => c.Register),
+  },
+
   {
     path: '',
     component: Layout,
     children: [
       { path: '', pathMatch: 'full', component: HomePage },
+
       {
-        path: 'courses',
-        canActivate: [authGuard],
-        loadComponent: () =>
-          import('./features/courses/courses.component').then((c) => c.CoursesComponent),
-      },
-      {
-        path: 'categories',
-        canActivate: [authGuard],
-        loadComponent: () =>
-          import('./features/categories-page/categories-page').then((c) => c.CategoriesPage),
-      },
-      {
-        path: 'courses/:id',
-        canActivate: [authGuard],
-        loadComponent: () =>
-          import('./features/course-detail/course-detail.component').then(
-            (c) => c.CourseDetailComponent,
-          ),
-      },
-      {
-        path: 'checkout',
-        canActivate: [authGuard],
-        loadComponent: () =>
-          import('./features/checkout/checkout/checkout').then((c) => c.Checkout),
-      },
-      {
-        path: 'checkout/success',
-        canActivate: [authGuard],
-        loadComponent: () => import('./features/checkout/success/success').then((c) => c.Success),
-      },
-      {
-        path: 'checkout/cancel',
-        canActivate: [authGuard],
-        loadComponent: () => import('./features/checkout/cancel/cancel').then((c) => c.Cancel),
-      },
-      {
-        path: 'my-courses',
-        canActivate: [authGuard],
-        loadComponent: () => import('./features/my-courses/my-courses').then((c) => c.MyCourses),
+        path: '',
+        canActivateChild: [authGuard],
+        children: [
+          {
+            path: 'courses',
+            children: [
+              {
+                path: '',
+                loadComponent: () =>
+                  import('./features/courses/courses.component').then((c) => c.CoursesComponent),
+              },
+              {
+                path: ':id',
+                loadComponent: () =>
+                  import('./features/course-detail/course-detail.component').then(
+                    (c) => c.CourseDetailComponent,
+                  ),
+              },
+            ],
+          },
+          {
+            path: 'categories',
+            loadComponent: () =>
+              import('./features/categories-page/categories-page').then((c) => c.CategoriesPage),
+          },
+          {
+            path: 'my-courses',
+            loadComponent: () =>
+              import('./features/my-courses/my-courses').then((c) => c.MyCourses),
+          },
+          {
+            path: 'checkout',
+            children: [
+              {
+                path: '',
+                loadComponent: () =>
+                  import('./features/checkout/checkout/checkout').then((c) => c.Checkout),
+              },
+              {
+                path: 'success',
+                loadComponent: () =>
+                  import('./features/checkout/success/success').then((c) => c.Success),
+              },
+              {
+                path: 'cancel',
+                loadComponent: () =>
+                  import('./features/checkout/cancel/cancel').then((c) => c.Cancel),
+              },
+            ],
+          },
+        ],
       },
 
       { path: '**', component: NotFound },
