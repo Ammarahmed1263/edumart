6a90d6c4fa36e5813bc98131f98d1f2d31fcbc08
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-05-19 19:29:39 +0300

    fix: loading and error state alignment and chatbot with unauth user

--- src/app/shared/components/layout/layout.ts
+++ src/app/shared/components/layout/layout.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChatbotComponent } from '../chatbot/chatbot';
 import { Footer } from '../footer/footer';
@@ -6,6 +6,7 @@ import { Navbar } from '../navbar/navbar';
 
 
 import { CartSyncService } from '../../../core/services/cart-sync.service';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-layout',
@@ -15,4 +16,8 @@ import { CartSyncService } from '../../../core/services/cart-sync.service';
 })
 export class Layout {
   private cartSyncService = inject(CartSyncService);
+  private authService = inject(AuthService);
+
+  showChatbot = computed(() => this.authService.isLoggedIn());
+
 }
